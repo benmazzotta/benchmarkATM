bcd146d65a0f8c9d517df8d03bebcc3b6e3c4095
Ben Mazzotta <benjamin.mazzotta@tufts.edu>
2014-09-25 16:29:22 -0400

    Assembled ATM, population and GDP per capita estimates.
    Also established interest rate multipliers.

--- scripts/master-import.R
+++ scripts/master-import.R
@@ -13,6 +13,7 @@
 #   2. IMF interest rates
 #   3. World Bank Population
 #   4. World Bank PPP conversion factors
+#   5. GDP per capita for spice
 
 
 #     0. Preliminaries
@@ -48,7 +49,23 @@ pop[,estimate:=rowMeans(.SD, na.rm=T), .SDcols=c(5:9)]
 summary(pop)
 
 
-#     3. PPP Conversion factors
+#     3. Price level ratios
+#          i.e., ratio of PPP to market exchange rate
+
+
+#       import the world bank CSV file
+ppplevel <- data.table(read.delim(file="ppplevel.csv", skip=2, header=T, sep=","))
+#       Keep only years 2008--2012
+ppplevel <- ppplevel[,c(1:4,54:58), with=F]
+str(ppplevel)
+
+#       Average years 2008--2012 "estimate"
+ppplevel[,estimate:=rowMeans(.SD, na.rm=T), .SDcols=c(5:9)]
+summary(ppplevel)
+with(ppplevel, table(Country.Name, estimate>1))
+
+
+##    I also did PPP conversion factors; not what we want
 
 pppconv <- read.delim(file="pppconversion.csv", skip=2, header=T, sep=",")
 pppconv <- data.table(pppconv)
@@ -98,27 +115,51 @@ summary(intrate)
 
 #     Keep just the first entry by Country and all four rates.
 names(intrate)
-intratebycountry <- unique(intrate[,c("Country", "deposit", "discount", "lending", "moneymarket"), with=F])
+interest <- unique(intrate[,c("Country", "deposit", "discount", "lending", "moneymarket"), with=F])
 
 #     Euro area discount rates reported by ECB
 #       Which countries?
 eurozone <- c("Austria","Belgium","Cyprus","Estonia","Finland","France","Germany","Greece","Ireland","Italy","Latvia","Luxembourg","Malta","Netherlands","Portugal","Slovakia","Slovenia","Spain")
 
 #       What rate?
-eurointerest <- intratebycountry["Euro Area",discount][[2]]
+eurointerest <- interest["Euro Area",discount][[2]]
 
 # #       Check the join/query works
-# intratebycountry[Country %in% eurozone & is.na(discount),]
+# interest[Country %in% eurozone & is.na(discount),]
 
 #       Replace missing values w/ eurozone discount rate
-intratebycountry[Country %in% eurozone & is.na(discount), discount:=eurointerest]
+interest[Country %in% eurozone & is.na(discount), discount:=eurointerest]
+
+#       US discount rate is the numeraire
+numeraire <- interest["United States", discount][[2]]
+interest[,estimate:=discount/numeraire]
+summary(interest)
+
+#       If the discount rate multiplier is unavailable ... 
+USmoneyrate <- interest["United States", moneymarket][[2]]
+interest[is.na(estimate),estimate := moneymarket/USmoneyrate]
+summary(interest)
+
+USlendingrate <- interest["United States", lending][[2]]
+interest[is.na(estimate),estimate := lending/USlendingrate]
+summary(interest)
+
+
 
 # #       Verify
-# intratebycountry[Country %in% eurozone,]
+# interest[Country %in% eurozone,]
 
-summary(intratebycountry)
+summary(interest)
 
 
+##      5. GDP per capita
+
+gdpcap <- read.delim(file="gdpcap.csv", sep=",", header=T, skip=2)
+gdpcap <- data.table(gdpcap)
+gdpcap <- gdpcap[,c(1:4, 54:58), with=F]
+str(gdpcap)
+gdpcap[,estimate:= rowMeans(.SD, na.rm=T), .SDcols=c(5:9)]
+summary(gdpcap)
 
 ## Save workspace to ~/data/current.Rdata
 
